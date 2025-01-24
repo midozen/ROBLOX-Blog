6c2940f183c85ccf9d36448e19679794c28512f3
mido <122419606+midozen@users.noreply.github.com>
2025-01-23 18:13:58 -0600

    Logging in on old browsers suppoer :P

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,11 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-
   adapter: node({
-    mode: "standalone",
+    mode: "standalone"
   }),
-
+  security: {
+    checkOrigin: false
+  },
   integrations: [svelte()],
 });
