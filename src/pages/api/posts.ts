335d8fdb814a8d57334fdc00872cd8a3912043f6
mido <122419606+midozen@users.noreply.github.com>
2025-03-14 13:05:29 -0500

    Update posts.ts

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,17 +1,26 @@
 import { prisma } from "@lib/prisma";
+import { getPostUrl } from "@utils/url";
 import type { APIContext } from "astro";
 
 export async function GET({ cookies, redirect }: APIContext) {
   const posts = await prisma.post.findMany({
     select: {
-      id: true,
       title: true,
+      slug: true,
+      dateCreated: true,
     },
     take: 4,
     orderBy: { dateCreated: "desc" },
   });
 
-  return new Response(JSON.stringify(posts), {
+  const siteUrl = import.meta.env.DEV ? "http://localhost:4321" : `http://blog.${import.meta.env.PUBLIC_WEBSITE_URL}`;
+
+  const formattedPosts = posts.map(post => ({
+    title: post.title,
+    link: `${siteUrl}${getPostUrl(post)}`,
+  }));
+
+  return new Response(JSON.stringify(formattedPosts), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
